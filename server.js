0d11a039a2cf4403a70b15b7942ea0b2c78f0382
Clark19 <clarkgable1@gmail.com>
2022-12-12 23:13:47 +0900

    Refactor: 26강 - 리팩토링

--- server.js
+++ server.js
@@ -2,46 +2,74 @@ var http = require("http");
 var fs = require("fs");
 const url = require("url");
 
-var app = http.createServer(function (request, response) {
+function templateHTML(title, list, body) {
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+`;
+}
+
+const templateList = (files) => {
+  let list = "<ul>";
+  files.forEach((file) => (list += `<li><a href="/?id=${file}">${file}</li>`));
+  list += "</ul>";
+  return list;
+};
+
+var app = http.createServer(async function (request, response) {
   var _url = request.url;
   let queryData = url.parse(_url, true).query;
-  let title = queryData.id;
-  console.log("쿼리스트링:", queryData);
-  if (_url == "/") {
-    // url = "/index.html";
-    title = "Welcome";
+  let pathname = url.parse(_url, true).pathname;
+
+  console.log("pathname:", pathname);
+  console.log("__dirname: ", __dirname);
+  console.log("process.argv: ", process.argv);
+
+  if (pathname === "/") {
+    if (!queryData.id) {
+      const title = "Welcome";
+      const description = "Hello, Node.js";
+      const files = await fs.readdirSync("./data");
+      if (!files) console.log("파일들 없음");
+      const list = templateList(files);
+      const template = templateHTML(
+        title,
+        list,
+        `<h2>${title}</h2>${description}`
+      );
+
+      response.writeHead(200);
+      response.end(template);
+    } else {
+      fs.readFile(`data/${queryData.id}`, "utf-8", async (err, description) => {
+        const title = queryData.id;
+        const files = await fs.readdirSync("./data");
+        if (!files) console.log("파일들 없음");
+        const list = templateList(files);
+        const template = templateHTML(
+          title,
+          list,
+          `<h2>${title}</h2>${description}`
+        );
+
+        response.writeHead(200);
+        response.end(template);
+        // response.end(fs.readFileSync(__dirname + url));
+      });
+    }
+  } else {
+    response.writeHead(404);
+    response.end("Not Found");
   }
-  if (_url == "/favicon.ico") {
-    return response.writeHead(404);
-  }
-
-  console.log(__dirname + "박");
-
-  fs.readFile(`data/${title}`, "utf-8", (err, description) => {
-    let template = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ul>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ul>
-      <h2>${title}</h2>
-      <p>${description}
-      </p>
-    </body>
-    </html>
-    `;
-
-    response.writeHead(200);
-    response.end(template);
-    // response.end(fs.readFileSync(__dirname + url));
-  });
 });
 app.listen(3000);
